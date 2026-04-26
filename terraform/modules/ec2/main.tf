2c80301d2b254a9ffa08b10bcdc1f71ed3a41d42
victor071997 <himanojkumar747@gmail.com>
2026-04-27 01:31:52 +0530

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -23,5 +23,6 @@ resource "aws_instance" "vm" {
   instance_type          = var.type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.sg.id]
+  associate_public_ip_address = true
   tags                   = { Name = "Terraform-VM" }
 }
